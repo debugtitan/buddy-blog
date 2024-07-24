fada887b2e322c95b64e5b3cb6d2e9a44f950b88
debugtitan <alts.devs@gmail.com>
2024-07-24 20:54:06 +0100

    cors initialization

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from core.db import Base,engine
+
+
+Base.metadata.create_all(engine)
 
 app = FastAPI(
   title="Buddy Blog API",
   description="api documentation for buddy-blog",
   version=""
+)
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
