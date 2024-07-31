35524583d8c0f251729c6ba1bcd3c0400cb69354
debugtitan <alts.devs@gmail.com>
2024-07-31 21:42:59 +0100

    blog router

--- core/routes/blogs.py
+++ core/routes/blogs.py
@@ -3,12 +3,10 @@ from core.db import db_dependacy
 from core.schemas.blogs import BlogRetrieve
 from core.models.blogs import Blogs
 
-blog_router = APIRouter(tags="Blog")
-
-blog_router.get("/blog", status_code=status.HTTP_200_OK)
+blog_router = APIRouter(tags=["Blogs"])
 
 
+@blog_router.get("/blogs", status_code=status.HTTP_200_OK)
 async def retrieve_blogs(db: db_dependacy) -> list[BlogRetrieve]:
-    """"""
     blog_items = db.query(Blogs).all()
-    print(blog_items)
+    return [BlogRetrieve.model_validate(blog) for blog in blog_items]
