efe2882bb902fd23aaf2dd691b12b565adb3a173
debugtitan <alts.devs@gmail.com>
2024-07-31 19:51:40 +0100

    blog tag enums

---
+++ core/utils/enums/blog.py
@@ -0,0 +1,18 @@
+from .base import BaseEnum
+
+
+class BlogTagType(BaseEnum):
+    TECH = "Technology"
+    DATA_SCIENCE = "Data S cience"
+    AI_ML = "AI / ML"
+    WEB_DEVELOPMENT = "Web Development"
+    MOBILE_DEVELOPMENT = "Mobile Development"
+    CLOUD_COMPUTING = "Cloud Computing"
+    DEVOPS = "DevOps"
+    CYBER_SECURITY = "Cyber Security"
+    DATABASES = "Databases"
+    SOFTWARE_ENGINEERING = "Software Engineering"
+    PROJECT_MANAGEMENT = "Project Management"
+    STARTUPS = "Startups"
+    ENTREPRENEURSHIP = "Entrepreneurship"
+    EVENTS = "EVENT", "Events"
