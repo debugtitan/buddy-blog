26631ea7ffef0bdb5b7e9e79b9cd5eba043c3c06
debugtitan <alts.devs@gmail.com>
2024-07-31 22:10:31 +0100

    change tags to enums

--- core/routes/blogs.py
+++ core/routes/blogs.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status
 from core.db import db_dependacy
 from core.schemas.blogs import BlogRetrieve, BlogCreate
 from core.models.blogs import Blogs
+from core.utils import enums
 
 blog_router = APIRouter(tags=["Blogs"])
 
@@ -14,7 +15,12 @@ async def retrieve_blogs(db: db_dependacy) -> list[BlogRetrieve]:
 
 @blog_router.post("/blogs", status_code=status.HTTP_201_CREATED)
 async def create_new_blog(db: db_dependacy, blog_data: BlogCreate):
-    new_blog = Blogs(**blog_data.model_dump())
+    new_blog = Blogs(
+        title=blog_data.title,
+        description=blog_data.description,
+        tag=enums.BlogTagType(blog_data.tag),
+        members_only=blog_data.members_only,
+    )
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
